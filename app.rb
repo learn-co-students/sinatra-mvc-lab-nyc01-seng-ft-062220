fd111aced5cc65a55c7c6bdfc4c92e27cbab8601
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-14 11:46:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        test1 = PigLatinizer.new
+        @string_piglatinized = test1.piglatinize(params["user_phrase"])
+
+        erb :results
+
+    end
 end
